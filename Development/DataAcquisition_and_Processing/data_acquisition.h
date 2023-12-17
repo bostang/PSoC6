b07f5c80039f8b00fd70fd9b84439df99d912161
ikbar-allaamfp <79163393+ikbar-allaamfp@users.noreply.github.com>
2023-12-17 15:49:31 +0700

    Update data_acquisition.h

--- Development/DataAcquisition_and_Processing/data_acquisition.h
+++ Development/DataAcquisition_and_Processing/data_acquisition.h
@@ -1,8 +1,8 @@
-#include "cyhal.h"
-#include "cybsp.h"
-#include "cy_retarget_io.h"
-
 #define RX_BUF_SIZE     4
-#define TX_BUF_SIZE     4
+#define TX_BUF_SIZE     13
+//#define TX_BUF_SIZE     14
+//#define TX_BUF_SIZE     5
 
-void data_acquisition_task(void *arg);
+//uint8_t data_acquisition_task(void *arg);
+//uint8_t* data_acquisition_task(void *arg);
+uint8_t* data_acquisition_task();
